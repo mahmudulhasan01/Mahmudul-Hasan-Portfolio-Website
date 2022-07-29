734811218817209f90df86ed54d37232d218816d
mahmudulhasan01 <mahmudulh479@gmail.com>
2022-07-29 22:53:06 +0600

    complete aboute me section

--- src/Components/Home/Services/Services.js
+++ src/Components/Home/Services/Services.js
@@ -1,9 +1,128 @@
 import React from "react";
+// import { pic } from "../../../assets/portfolio1.jpg";
+import pic from "../../../assets/portfolio5.png";
 
 const Services = () => {
   return (
     <section id="services">
-      <h6>Services</h6>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+      <div className="container portfolio_container">
+        <article className="portfolio_item">
+          <div className="portfolio_item-image">
+            <img src={pic} alt="" />
+          </div>
+
+          <h3>This is a portfolio item title</h3>
+
+          <a href="https://github.com" className="btn" target="_blank">
+            {" "}
+            Github{" "}
+          </a>
+          <a
+            href="https://github.com"
+            className="btn btn-primary"
+            target="_blank"
+          >
+            Live Demo
+          </a>
+        </article>
+        <article className="portfolio_item">
+          <div className="portfolio_item-image">
+            <img src={pic} alt="" />
+          </div>
+
+          <h3>This is a portfolio item title</h3>
+
+          <a href="https://github.com" className="btn" target="_blank">
+            {" "}
+            Github{" "}
+          </a>
+          <a
+            href="https://github.com"
+            className="btn btn-primary"
+            target="_blank"
+          >
+            Live Demo
+          </a>
+        </article>
+        <article className="portfolio_item">
+          <div className="portfolio_item-image">
+            <img src={pic} alt="" />
+          </div>
+
+          <h3>This is a portfolio item title</h3>
+
+          <a href="https://github.com" className="btn" target="_blank">
+            {" "}
+            Github{" "}
+          </a>
+          <a
+            href="https://github.com"
+            className="btn btn-primary"
+            target="_blank"
+          >
+            Live Demo
+          </a>
+        </article>
+        <article className="portfolio_item">
+          <div className="portfolio_item-image">
+            <img src={pic} alt="" />
+          </div>
+
+          <h3>This is a portfolio item title</h3>
+
+          <a href="https://github.com" className="btn" target="_blank">
+            {" "}
+            Github{" "}
+          </a>
+          <a
+            href="https://github.com"
+            className="btn btn-primary"
+            target="_blank"
+          >
+            Live Demo
+          </a>
+        </article>
+        <article className="portfolio_item">
+          <div className="portfolio_item-image">
+            <img src={pic} alt="" />
+          </div>
+
+          <h3>This is a portfolio item title</h3>
+
+          <a href="https://github.com" className="btn" target="_blank">
+            {" "}
+            Github{" "}
+          </a>
+          <a
+            href="https://github.com"
+            className="btn btn-primary"
+            target="_blank"
+          >
+            Live Demo
+          </a>
+        </article>
+        <article className="portfolio_item">
+          <div className="portfolio_item-image">
+            <img src={pic} alt="" />
+          </div>
+
+          <h3>This is a portfolio item title</h3>
+
+          <a href="https://github.com" className="btn" target="_blank">
+            {" "}
+            Github{" "}
+          </a>
+          <a
+            href="https://github.com"
+            className="btn btn-primary"
+            target="_blank"
+          >
+            Live Demo
+          </a>
+        </article>
+      </div>
     </section>
   );
 };
